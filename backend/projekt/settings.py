de39d19fef760704a88744c133b33fd71265e00d
Borna Matkovic <borna.matkovic@fer.hr>
2026-06-04 18:20:21 +0200

    a

--- backend/projekt/settings.py
+++ backend/projekt/settings.py
@@ -95,6 +95,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5174',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
+    'https://kognis-opal.vercel.app',
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
